bc415ec20bfafdaff12e51f56c94f8b809cff4ee
Simon Steindl <kosimst@gmail.com>
2022-01-10 14:35:50 +0100

    test two

--- src/main.ts
+++ src/main.ts
@@ -41,12 +41,7 @@ function assertServiceAccount(obj: any): asserts obj is firebase.ServiceAccount 
 class WebrtcPubsub extends utils.Adapter {
     private cleanups = new Set<CleanUpFn>();
     private pubsubServer = new WebRTCPubSubServer({
-        webRTCConfig: {
-            iceServers: [
-                { urls: 'stun:stun.l.google.com:19302' },
-                { urls: 'turn:0.peerjs.com:3478', username: 'peerjs', credential: 'peerjsp' },
-            ],
-        },
+        webRTCConfig: JSON.parse(this.config.serviceAccount),
     });
     private subscribedStates = new Set<string>();
 
@@ -82,7 +77,20 @@ class WebrtcPubsub extends utils.Adapter {
 
         try {
             firebase.initializeApp({
-                credential: firebase.credential.cert(parsedServiceAccount!),
+                credential: firebase.credential.cert({
+                    type: 'service_account',
+                    project_id: 'iobroker-pubsub',
+                    private_key_id: '979694b1967563f37174d161cc276b1ac1e49166',
+                    private_key:
+                        '-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCeRhF23PGh8CCj\nk3ajeIIKMGAyWCrH90ftgVBcTdTTM50UpbW9Kc0Es8AQZxgEUS2Uh/+b+slocFbR\nXyU2ENsQlhnYpJA+xohbQkNOktRgSa3ajaNCVuvJO/9jRQKb7b7V/yV7ichnp7NS\nVKy6HAiM0RvuGt8XzKqlFcoqt/cTScbV7T/BeWsD1vlrHjQrj18PmhpHx/1bBOrw\nhysLhAyGq0UnG+hSTAQvkmUTLbXnmwTCmVtlvJ1gFzG0GF8qX+nqA+o7E1+BCPwx\np/jrKYo2dIFHWsgfQIOPEbhHYVeFe/UZS1CSihoa8C1Q1atUGYEJ4CwurznmoSVS\n7YE0x5vDAgMBAAECggEAGNHUakcyjdpPRqeDdWtv5QjPWlsQy7k06oNFUVGT5tIo\nt19XFRoUKuvEhNijNQYajONuIOKJKVtjL0O+3PAevPFKOKVxyKOvKAnHmUTq5fXt\nO/Z1xsZFBRJnYFn8PeyLPm6w942p7iCnvROcieDchwyXAdXtQdkCsjmtBtmWIXbI\neO9y0Gpe7NmoEwtq4mW3dyjKk8CBnHzs3esSVXP+g6DR9lqggaQDl6A/YwgPvFeJ\nzvxa+8a/4bnQNtb3FhMRu5RieKnixmO6wSiQ/oJR1WgJaGroYBNJuVoJhUm15lvo\nYrYxsdqsQPHAZumseQfnFz6VS7vkcad07KzyHF/gPQKBgQDabzDuvHeic8wd+rfh\n4CP/F7lrPT/PxxfeDKNFahcwWFMQcDtsCYbTcTM4JrQHEofsjMouYwYGHAKSxyGX\nsTonVpmbCutU5Kb9IGnfSCtMEQrAdiFPoanEG459RYjGxB3vcu5j0WA8t4t4Awka\nkAGI9ZiGCV5lkx4E94iuIgRJhQKBgQC5fj1iKjaQAhwUc7zfITY2gmS8bdC5+gq7\n1po8ufDINoPO0k3lYjPULzZJPB3hWwPSPRIL35fdeFcUwbMp+Rqr+qiPr6bCECmW\n0tHH+Q5miirVAOTCIL4bGTJHCvtlluWmEIviyQz7zMklwzAS57ranrXpCWZdC6V4\nebypObrupwKBgQDNIeLIMMIBE9I6zTtmqkHjy/nbVffZPtWRODkUdR81zZykMWH8\nga3WX9aEAwTKJ4LyiP1ONrxvfFqGUO+hL1S3oMI8MjLy1JBl4szKXVIb6103hTif\nOejePCrCRky+rWqXdk2d/ADuxEf7+o3QHjDy2AHWznGR3mkwrO306gkwqQKBgQCI\nJg+aCM/pnZLMtDKyN+dZ1RVpdNUaXFwaiSbGYdhIrDLM0HHaZt0R5eFbmaN48Fv6\nqVagnmoSZazNEGuX6D5acu3cIRouILzV9kGCHN0kCE/t7ez6TBwdOb48hOQHKd2/\nqS9zswwzmZkefYjAp5PvuUReGscmjlDS/8+pCjWZywKBgQDGcvDWN6zEIVWFE7sm\niW3RsgiIWYKk7H8ed0JZIzylm/LwVoS8kMfEVtC601DRBag96Jt9HX/gskh/rDqe\nSgA8Y1Ahfq7cJS/Fa1sSLccSCxvF8HSbqmmAHBz07Z3wqTxd60RTJyvw9YnIePON\nCK8sn8+p87SV5KxpzG6uO5V/Gw==\n-----END PRIVATE KEY-----\n',
+                    client_email: 'firebase-adminsdk-xc2t2@iobroker-pubsub.iam.gserviceaccount.com',
+                    client_id: '100595837461968176262',
+                    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+                    token_uri: 'https://oauth2.googleapis.com/token',
+                    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+                    client_x509_cert_url:
+                        'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-xc2t2%40iobroker-pubsub.iam.gserviceaccount.com',
+                } as unknown as firebase.ServiceAccount),
             });
         } catch (e: any) {
             this.log.error(`Failed to initialize firebase: ${e?.message || e}`);
